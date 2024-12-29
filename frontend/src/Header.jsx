40367f19111da297c31fb2cf9db7b6e552e3779b
Dishma Gangulel <120539966+gangulel@users.noreply.github.com>
2024-12-29 15:31:30 +0530

    int

--- frontend/src/Header.jsx
+++ frontend/src/Header.jsx
@@ -1,12 +1,17 @@
 import './header.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser } from '@fortawesome/free-solid-svg-icons'
+import { faBox } from '@fortawesome/free-solid-svg-icons'
+import { faClipboard } from '@fortawesome/free-solid-svg-icons'
+
 function Header() {
   return (
     <div>
       <header className="App-header">
       <div className="navbar">
-        <a href="#Current Order">Current Order</a>
-        <a href="#Employees">Employees</a>
-        <a href="#Order History">Order History</a>
+        <a href="#Current Order"><FontAwesomeIcon icon={faClipboard} />  Current Order</a>
+        <a href="#Employees"><FontAwesomeIcon icon={faUser} />   Employees</a>
+        <a href="#Order History"><FontAwesomeIcon icon={faBox} />  Order History</a>
       </div>
       </header>
     </div>
