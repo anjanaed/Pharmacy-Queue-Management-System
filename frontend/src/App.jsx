487e11cb9792af9f09202367f2d6481af5dbf5fe
Anjana Edirisinghe <anjanaape@gmail.com>
2025-01-03 03:09:12 +0530

    master merged

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,10 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/login";
 import Register from "./components/register";
 import User from "./components/user";
+import Employees from "./components/Employees"
 import EmployeeInterface from "./components/EmployeeInterface";
 
 function App() {
@@ -14,7 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Register />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/user" element={<EmployeeInterface />} />
+        <Route path="/user" element={<Employees />} />
       </Routes>
     </Router>
   );
